93a04f35ac779461b002cc74ad01be9c00f14464
Be4Die <misha375375@gmail.com>
2026-08-07 15:01:14 +0400

    style: make wlogout compact with 3 columns and larger margins

--- hypr/.config/hypr/powermenu.sh
+++ hypr/.config/hypr/powermenu.sh
@@ -6,6 +6,6 @@ if pgrep -x "wlogout" > /dev/null
 then
     killall wlogout
 else
-    # Start wlogout with Frappe theme
-    wlogout -b 2 -c 0 -r 0 -m 0 --layout ~/.config/wlogout/layout --css ~/.config/wlogout/style.css
+    # Start wlogout with Frappe theme, compact grid (3 columns) and large margins
+    wlogout -b 3 -c 40 -r 40 -T 300 -B 300 -L 300 -R 300 --layout ~/.config/wlogout/layout --css ~/.config/wlogout/style.css
 fi
